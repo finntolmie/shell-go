1e33b6dbd24019607da1d1b6ffc5a04c379be5e7
finntolmie <finn.tolmie12@gmail.com>
2024-12-30 10:58:11 +1100

    2. invalid commands

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,10 +4,25 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
+
+func cleanInput() string {
+	for {
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading user input: %s\n", err.Error())
+			continue
+		}
+		return strings.TrimSpace(input)
+	}
+}
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
-
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	input := cleanInput()
+	switch input {
+	default:
+		fmt.Fprintf(os.Stderr, "%s: command not found\n", input)
+	}
 }
