e94174d6948aebfb318a0ab23b950c724dc3fa54
finntolmie <finn.tolmie12@gmail.com>
2024-12-30 11:08:42 +1100

    4. exit builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,27 +4,47 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func cleanInput() string {
+type Command struct {
+	Args []string
+}
+
+func NewCommand() Command {
+	var cmd Command
 	for {
 		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error reading user input: %s\n", err.Error())
 			continue
 		}
-		return strings.TrimSpace(input)
+		cmd.Args = strings.Fields(input)
+		return cmd
 	}
 }
 
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		input := cleanInput()
-		switch input {
+		cmd := NewCommand()
+		if len(cmd.Args) == 0 {
+			continue
+		}
+		switch cmd.Args[0] {
+		case "exit":
+			var exitCode int = 0
+			var err error
+			if len(cmd.Args) > 1 {
+				exitCode, err = strconv.Atoi(cmd.Args[1])
+				if err != nil {
+					exitCode = 1
+				}
+			}
+			os.Exit(exitCode)
 		default:
-			fmt.Fprintf(os.Stderr, "%s: command not found\n", input)
+			fmt.Fprintf(os.Stderr, "%s: command not found\n", cmd.Args[0])
 		}
 	}
 }
