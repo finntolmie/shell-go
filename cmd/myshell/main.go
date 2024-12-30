60d80ad4fdaaa304e65ba88fb76253478261165c
finntolmie <finn.tolmie12@gmail.com>
2024-12-30 11:22:40 +1100

    5. echo and builtin refactor

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,6 +8,33 @@ import (
 	"strings"
 )
 
+var exitCode = 0
+
+func exitBuiltin(cmd *Command) bool {
+	var errno int = 0
+	var err error
+	if len(cmd.Args) > 1 {
+		errno, err = strconv.Atoi(cmd.Args[1])
+		if err != nil {
+			errno = 1
+		}
+	}
+	exitCode = errno
+	return false
+}
+
+func echoBuiltin(cmd *Command) bool {
+	if len(cmd.Args) > 1 {
+		fmt.Fprintf(os.Stdout, "%s\n", strings.Join(cmd.Args[1:], " "))
+	}
+	return true
+}
+
+var builtins = map[string]func(*Command) bool{
+	"exit": exitBuiltin,
+	"echo": echoBuiltin,
+}
+
 type Command struct {
 	Args []string
 }
@@ -26,25 +53,18 @@ func NewCommand() Command {
 }
 
 func main() {
-	for {
+	var repl bool = true
+	for repl {
 		fmt.Fprint(os.Stdout, "$ ")
 		cmd := NewCommand()
 		if len(cmd.Args) == 0 {
 			continue
 		}
-		switch cmd.Args[0] {
-		case "exit":
-			var exitCode int = 0
-			var err error
-			if len(cmd.Args) > 1 {
-				exitCode, err = strconv.Atoi(cmd.Args[1])
-				if err != nil {
-					exitCode = 1
-				}
-			}
-			os.Exit(exitCode)
-		default:
-			fmt.Fprintf(os.Stderr, "%s: command not found\n", cmd.Args[0])
+		if builtin, ok := builtins[cmd.Args[0]]; ok {
+			repl = builtin(&cmd)
+			continue
 		}
+		fmt.Fprintf(os.Stderr, "%s: command not found\n", cmd.Args[0])
 	}
+	os.Exit(exitCode)
 }
